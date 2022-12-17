b3f13fd3df667ab43849e80b99ceea1ae41b1b1c
soniSUHANI <114800693+soniSUHANI@users.noreply.github.com>
2022-12-17 22:02:22 +0530

    Add files via upload

---
+++ setnotes.py
@@ -0,0 +1,21 @@
+# set= store multiple values in single variable
+# unordered
+# mySet ={}
+# unchangeble/immutable
+# duplicates not allowed
+# non homogenous
+mySet1={"car","boat","train"}
+mySet2={1,2,3,4}
+mySet3={4,5,6,7}
+#mySet1.update(mySet2)
+#mySet1.add("Audy")
+#mySet1.discard("car")
+#mySet1.pop()
+# output=mySet2.union(mySet3)
+# print(output)
+output=mySet2.intersection(mySet3)
+output1=mySet2.symmetric_difference(mySet3)
+print(output)
+print(output1)
+
+
